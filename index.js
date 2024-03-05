42ea935c68ee768071504b93974e442b5dd087d5
Dominique Gibson <DGibson8822@gmail.com>
2024-03-05 05:39:16 -0600

    Added route for updating a character.

--- index.js
+++ index.js
@@ -76,6 +76,48 @@ app.get('/characters/:id', async (req, res) => {
     }
 })
 
+// update
+app.put('/characters/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { 
+            character_description,
+            character_type,
+            avatar_style,
+            top_type,
+            accessories_type,
+            hair_color,
+            facial_hair_type,
+            clothe_type,
+            clothe_color,
+            eye_type,
+            eyebrow_type,
+            mouth_type,
+            skin_color
+        } = req.body;
+        const updateCharacter = await pool.query(
+            'UPDATE character SET character_description = $1, character_type = $2, avatar_style = $3, top_type = $4, accessories_type = $5, hair_color = $6, facial_hair_type = $7, clothe_type = $8, clothe_color = $9, eye_type = $10, eyebrow_type = $11, mouth_type = $12, skin_color = $13 WHERE character_id = $14',
+            [character_description,
+                character_type,
+                avatar_style,
+                top_type,
+                accessories_type,
+                hair_color,
+                facial_hair_type,
+                clothe_type,
+                clothe_color,
+                eye_type,
+                eyebrow_type,
+                mouth_type,
+                skin_color,
+                id]
+        );
+        res.status(200).send(`Character with id = ${id} was updated.`);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
 app.listen(5000, () => {
     console.log('Disney API Server has started on port 5000.')
 });
